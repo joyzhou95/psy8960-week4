15aab519f70edac9d71a71187b920d0f64979ef3
Joy Zhou <zhou1559@umn.edu>
2023-02-10 11:26:11 -0600

    Convert 0 to NA for all question responses

--- R/week4.R
+++ R/week4.R
@@ -5,7 +5,8 @@ library(tidyverse)
 #Data Import
 import_tbl <- read_delim(file = "../data/week4.dat.txt", delim = "-", col_names = c("casenum","parnum","stimver","datadate","qs"))
 glimpse(import_tbl)
-wide_tbl <- separate(import_tbl, qs, c("q1", "q2", "q3", "q4", "q5"))
+wide_tbl <- separate(import_tbl, qs, into = c("q1", "q2", "q3", "q4", "q5"))
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
-  
+wide_tbl[,5:9] <- lapply(wide_tbl[,5:9], gsub, pattern = 0, replacement = "NA")
+

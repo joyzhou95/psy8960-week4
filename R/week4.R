f15c1ca296f53c96171431314c3525122007c070
Joy Zhou <zhou1559@umn.edu>
2023-02-09 21:37:39 -0600

    Split the question column into 5 different columns

--- R/week4.R
+++ R/week4.R
@@ -5,3 +5,4 @@ library(tidyverse)
 #Data Import
 import_tbl <- read_delim(file = "../data/week4.dat.txt", delim = "-", col_names = c("casenum","parnum","stimver","datadate","qs"))
 glimpse(import_tbl)
+wide_tbl <- separate(import_tbl, qs, c("q1", "q2", "q3", "q4", "q5"))
